aed68b2c5e44ddc870e27a15bfd1b14c063ffcb4
David Chung <david23@my.yorku.ca>
2020-04-28 23:02:51 -0400

    Updates

---
+++ precache-manifest.c856e3e2b825f07f219e21678152755d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504ae2ee62947c49489f8a156d41fdf9",
+    "url": "/MyToDoList/index.html"
+  },
+  {
+    "revision": "13bbb0ab69de7cdea46c",
+    "url": "/MyToDoList/static/css/main.4feb377a.chunk.css"
+  },
+  {
+    "revision": "4badc753de1cfd215dc4",
+    "url": "/MyToDoList/static/js/2.260d310e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyToDoList/static/js/2.260d310e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bbb0ab69de7cdea46c",
+    "url": "/MyToDoList/static/js/main.42959344.chunk.js"
+  },
+  {
+    "revision": "40ae1af74270a8199bb6",
+    "url": "/MyToDoList/static/js/runtime-main.8d88ce41.js"
+  }
+]);
